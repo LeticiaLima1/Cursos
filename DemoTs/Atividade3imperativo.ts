298368410c34bad9e6ea512a0f52607fdd912e77
Letícia Lima <advleticialimaferreira@gmail.com>
2021-11-11 20:05:42 -0300

    atividade3

--- DemoTs/Atividade3imperativo.ts
+++ DemoTs/Atividade3imperativo.ts
@@ -1,9 +1,6 @@
-const arrayNumeros = [20, 30, 40, 50]; //array de números
-const maximoValor = Math.max.apply(null, arrayNumeros);//constante para obter o valor máximo
-const minimoValor = Math.min.apply(null, arrayNumeros);//constante para obter o valor mínimo
-const soma = arrayNumeros.reduce((a, b) => a + b, 0);//constante usando o reduce para obter a soma dos elementos
-const media = soma / arrayNumeros.length; //usar soma e divisão pelos elementos e obter a média 
+const arrayNumeros: Array<number> = [20, 30, 40, 50]; //array de números
 
-console.log(maximoValor); //impressão valor máximo
-console.log(minimoValor); //impressão valor mínimo
-console.log(media); //impressão média
+const listaOrganizada: Array<number> = [Math.max.apply(null, arrayNumeros), Math.min.apply(null, arrayNumeros), arrayNumeros.reduce((a, b) => a + b, 0) / arrayNumeros.length ]
+//declara a função, tipa a Lista e função para encontrar máximo, mínimo e média.
+
+console.log(listaOrganizada);//impressão da nova lista
